9f1ad0b23ae7c60dfa35c1581c58f7fb4e768fc3
itdanja <itdanja@kakao.com>
2024-09-12 12:19:57 +0900

    day89

--- src/main/resources/static/js/chat/chat.js
+++ src/main/resources/static/js/chat/chat.js
@@ -1,7 +1,17 @@
 console.log('chat.js')
+// --- 비회원제 , 익명 식별이름 생성
+    // Math.random()                    :  0 ~ 1 사이의 난수
+    // Math.random() * 끝값(미만)        :  0 ~ 끝값 사이의 난수
+    // ( Math.random() * 끝값 ) + 시작값 :  시작값 ~ 끝값 사이의 난수
+    // Math.floor( )                   :  소수점 제거 함수
+let randomNo = Math.floor( Math.random() * 1001) + 1
+let nickName = `익명${randomNo}`
+console.log(nickName)
+
 // JS 클라이언트 웹소켓 # new WebSocket("ws://localhost:8080/ws매핑주소")
 let clientSocket = new WebSocket("ws://192.168.30.9:8080/chat/conn")
 console.log( clientSocket );
+
 // [1] clientSocket 의 onclose , onerror , onmessage , onopen 정의 해야한다.
     // (1) WebSocket객체내 onopen 속성은 서버소켓과 접속을 성공했을때 발동되는 함수 정의해서 대입
 clientSocket.onopen = ( e ) => {
@@ -21,9 +31,37 @@ clientSocket.onmessage = ( messageEvent ) => { // e : 매개변수
 function onMsgSend(){
     //
     let msgInput = document.querySelector('.msgInput')
+    // - 메시지 내용 들을 객체 형식으로 구성
+    let msg = {
+        'message' : msgInput.value ,
+        'from' : nickName ,
+        'date' : new Date().toLocaleString()
+    }
     // - 현재 클라이언트소켓과 연결 유지된 서버소켓에게 메시지 전송 # .send( ) : 서버소켓에게 메시지 전송
-    clientSocket.send( msgInput.value );
+    clientSocket.send( JSON.stringify( msg )  );
+        // msg ----> [object Object]
+        // JSON.stringify( msg ) ---> {"message":"DFGDFG","from":"익명209","date":"2024. 9. 12. 오후 12:18:21"}
+
+        // - JSON.stringify( js객체  ) : js객체타입(JSON형식) --> 문자열타입(JSON형식)
+        // -  "3"(문자열타입 숫자형식) VS 3(정수타입 숫자형식)
+        // -  { key : value } (객체타입 JSON형식 )  vs   "{ key : value }" ( 문자열타입 JSON형식 )
     //
     msgInput.msgInput = "";
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
